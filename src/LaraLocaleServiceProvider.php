1017ac19340e1f5732a3e952d64f5c7a2211d728
Vladyslav Lopatin <lopatin@wskz.pl>
2025-03-12 15:00:32 +0100

    Fix spacing in file path concatenation for consistency
    Updated file path concatenations to include spaces around the dot operator for improved readability and coding standard consistency. No functional changes were made to the code.

--- src/LaraLocaleServiceProvider.php
+++ src/LaraLocaleServiceProvider.php
@@ -16,24 +16,25 @@ class LaraLocaleServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/lara-locale.php' => config_path('lara-locale.php'),
+            __DIR__ . '/../config/lara-locale.php' => config_path('lara-locale.php'),
         ], 'lara-locale-config');
 
         $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'lara-locale-migrations');
 
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'lara-locale');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'lara-locale');
 
         $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/lara-locale'),
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/lara-locale'),
         ], 'lara-locale-lang');
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'lara-locale');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'lara-locale');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/lara-locale'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/lara-locale'),
         ], 'lara-locale-views');
-
+    }
+}
